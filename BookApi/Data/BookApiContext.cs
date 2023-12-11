d1a6074e6c048268f9339e9d4f6f1591d5f785a1
MikesSzinyei <szinyeimikes@gmail.com>
2023-12-11 11:03:08 +0100

    first commit

---
+++ BookApi/Data/BookApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookApi.Models;
+
+namespace BookApi.Data
+{
+    public class BookApiContext : DbContext
+    {
+        public BookApiContext (DbContextOptions<BookApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookApi.Models.Book> Book { get; set; } = default!;
+    }
+}
